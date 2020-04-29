67869daa5e5b12d59d9cab4ecc92dd245496e121
Clayton Hadaway <me@hadaclay.com>
2020-04-28 19:10:29 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Quake
- */
 public class Main extends Application {
     
     @Override
